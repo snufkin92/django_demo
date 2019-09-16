63d174b49d474e6605290617621c27f473156b01
snufkin92 <shiroshi92@gmail.com>
2019-09-16 14:23:35 +0900

    update:ディレクトリの移動 mysiteディレクトリの重複を解消

---
+++ polls/views.py
@@ -0,0 +1,27 @@
+from django.views import generic
+
+from .models import Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    model = Question
+    template_name = 'polls/results.html'
